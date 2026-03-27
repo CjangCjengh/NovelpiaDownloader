c9b62ab1a8648d2f26b437636b48a19837f28a9e
kimms9606 <153983665+kimms9606@users.noreply.github.com>
2026-03-28 00:44:19 +0900

    Update Novelpia.cs

--- Novelpia.cs
+++ Novelpia.cs
@@ -25,6 +25,7 @@ namespace NovelpiaDownloader
             request.UserAgent = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Mobile Safari/537.36";
             request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
             request.Headers.Add("cookie", $"LOGINKEY={loginkey};");
+            request.Referer = "https://novelpia.com/";
             using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
                 streamWriter.Write($"redirectrurl=&email={id}&wd={pw}");
